124c17d94acc0f70837db89ef0946d435672f4cc
Lowcost42 <160510760+Lowcost42@users.noreply.github.com>
2024-03-13 09:43:11 +0100

    ProjectSection maj
    Maj de la section projets, fonctionnel

--- app/components/ProjectCard.jsx
+++ app/components/ProjectCard.jsx
@@ -3,11 +3,12 @@ import React from 'react'
 const ProjectCard = ({ imgUrl, title, description }) => {
   return (
     <div>
-      <div className='h-52 md:h-72' style={{ background: `url(${imgUrl})`, backgroundSize: "cover" }}>
+      <div className='h-52 md:h-72 rounded-t-xl'
+        style={{ background: `url(${imgUrl})`, backgroundSize: "cover" }}>
       </div>
-      <div className='text-white'>
-        <h5>{title}</h5>
-        <p>{description}</p>
+      <div className='text-white rounded-b-xl bg-[#181818] py-6 px-4'>
+        <h5 className='font-xl font-semibold mb-2'>{title}</h5>
+        <p className='text-[#ABD7BE]'>{description}</p>
       </div>
     </div>
   )
--- app/components/ProjectSection.jsx
+++ app/components/ProjectSection.jsx
@@ -7,48 +7,54 @@ const projectsData = () => [
         title: "Site Vitrine",
         description: "Mon tout premier site vitrine fait en HTML / CSS en première année de BTS SIO",
         image: "/images/Projects/1.png",
-        tag: ["All", "Web"]
+        tag: ["Tout", "Web"]
     },
     {
         id: 2,
-        title: "Site dynamique MVC",
+        title: "Site dynamique MVC / Bootstrap",
         description: "Un framework crée de A à Z avec le modèle MVC",
         image: "/images/Projects/2.png",
-        tag: ["All", "Web"]
+        tag: ["Tout", "Web"]
     },
     {
         id: 3,
         title: "Site dynamique no-MVC",
         description: "Mon projet personnel de ma deuxième année de BTS SIO",
         image: "/images/Projects/3.png",
-        tag: ["All", "Web"]
+        tag: ["Tout", "Web"]
     },
     {
         id: 4,
-        title: "Jeu Pong",
+        title: "Jeu Pong avec LOVE2D",
         description: "Création du jeu 'Pong' en Lua sur LOVE2D",
         image: "/images/Projects/4.png",
-        tag: ["All", "Game"]
+        tag: ["Tout", "Game"]
     },
     {
         id: 5,
-        title: "Site web Portfolio",
+        title: "Site web Portfolio avec Next.js",
         description: "Création de mon site de Portfolio en ligne",
         image: "/images/Projects/5.png",
-        tag: ["All", "Web"]
+        tag: ["Tout", "Web"]
     }
 ]
 
 const ProjectSection = () => {
     return (
         <>
-            <h2>Mes Projets</h2>
-            <div>{projectsData.map((project) =>
-                <ProjectCard key={project.id}
-                    title={project.title}
-                    description={project.description}
-                    imgUrl={project.image} />
-            )}
+            <h2 className='text-center text-4xl font-bold text-white'>
+                Mes Projets
+                </h2>
+            <div className='grid md:grid-cols-3 gap-8 md:gap-12'>
+                {projectsData().map((project) => (
+                    <ProjectCard
+                        key={project.id}
+                        title={project.title}
+                        description={project.description}
+                        imgUrl={project.image}
+                        tags={project.tag}
+                    />
+                ))}
             </div>
         </>
     );
