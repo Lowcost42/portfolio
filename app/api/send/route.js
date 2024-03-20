1d4cbc42fe1e37b70190dd8f7ad18c32f3177c80
Lowcost_42 <dede.tfca@gmail.com>
2024-03-20 21:41:29 +0100

    deployed

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -4,26 +4,28 @@ import { Resend } from "resend";
 const resend = new Resend(process.env.RESEND_API_KEY);
 const fromEmail = process.env.FROM_EMAIL;
 
+// console.log(resend);
+// console.log(fromEmail);
+
 export async function POST(req, res) {
-    const { email, subject, message } = await req.json();
-    console.log(email, subject, message);
+    const { email, objet, message } = await req.json();
+    console.log(email, objet, message);
     try {
         const data = await resend.emails.send({
             from: fromEmail,
-            to: [fromEmail, email],
-            subject: subject,
+            to: email,
+            objet: objet,
             react: (
                 <>
-                    <h1>{subject}</h1>
+                    <h1>{objet}</h1>
                     <p>Thank you for contacting us!</p>
                     <p>New message submitted:</p>
                     <p>{message}</p>
                 </>
-            ),
+            )
         });
-        return NextResponse.json(data);
+        return Response.json(data);
     } catch (error) {
-        return NextResponse.json({ error });
+        return Response.json({ error });
     }
 }
-
