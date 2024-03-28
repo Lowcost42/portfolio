71f1d0fb731eceb6ace65da4b5c18d6c6d3210a1
Lowcost_42 <dede.tfca@gmail.com>
2024-03-28 19:59:46 +0100

    cv

--- app/components/AboutSection.jsx
+++ app/components/AboutSection.jsx
@@ -53,7 +53,7 @@ const AboutSection = () => {
         });
     }
     return (
-        <section className='text-white'>
+        <section className='text-white' id='#a_propos'>
             <div className='md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16'>
                 <Image src="/images/about-img.png" alt="image à propos" width={500} height={500} />
                 <div className='mt-4 md:mt-0 text-left flex flex-col h-full'>
